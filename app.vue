d41877ea7d93bfafa719a835ae38e371d877566b
BayBreezy <behon.baker@yahoo.com>
2024-01-07 07:13:59 -0500

    chore: move classes from sidebar to parent

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
     <Header />
     <div class="container lg:grid lg:grid-cols-12 lg:gap-10">
       <!-- Sidebar -->
-      <Sidebar class="hidden lg:col-span-3 lg:block" />
+      <Sidebar class="sticky top-[65px] hidden h-[calc(100vh-65px)] py-5 lg:col-span-3 lg:block" />
 
       <!-- Main Section -->
       <main class="py-5 lg:col-span-6">
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="sticky top-[65px] h-[calc(100vh-65px)] py-5">
+  <aside>
     <div
       class="flex h-full w-full flex-col gap-5 overflow-y-auto rounded-md border bg-card pb-5 scrollbar-thin scrollbar-thumb-input scrollbar-thumb-rounded-md"
     >
